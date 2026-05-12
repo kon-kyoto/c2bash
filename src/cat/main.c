c44909016dde0520b3a12eaff3a4450ec007233e
kon-kyoto <kyotodanmaster@gmail.com>
2026-05-12 10:15:00 +0000

    feat: add help flag to cat

--- src/cat/main.c
+++ src/cat/main.c
@@ -1,10 +1,16 @@
 #include <stdio.h>
+#include <string.h>
 
 FILE* readFile(char* file);
 void returnContent(FILE* f, int flags[5]);
 
-int main() {
+int main(int argc, char* argv[]) {
 	// 5 flags [ number-nonblanck, end-point, number, squeeze-blank, tab-point ]
+	if ( argc >= 2 && strcmp(argv[1], "--help") == 0) {
+		printf("./cat [OPTIONS] [FILE]\n");
+		return 0;
+	}
+
 	int flags[5] = {0, 0 ,0 ,0 ,0};
 	FILE *f = readFile("test.txt");
 	if(f) {
