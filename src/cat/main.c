ec1b66c9e7dfb35ddd95b2492e50d88b9e9cde47
kon-kyoto <kyotodanmaster@gmail.com>
2026-05-12 10:49:57 +0000

    feat: added logic for flag from argument cat prog

--- src/cat/main.c
+++ src/cat/main.c
@@ -1,12 +1,40 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
 FILE* readFile(char* file);
 void returnContent(FILE* f, int flags[5]);
+void exitWithHelp();
 
-int main() {
+int main(int argc, char* argv[]) {
 	// 5 flags [ number-nonblanck, end-point, number, squeeze-blank, tab-point ]
-	int flags[5] = {0, 0 ,0 ,0 ,0};
-	FILE *f = readFile("Makefile");
+	int flags[5] = {0};
+	FILE* f = NULL;
+	
+	if ( argc < 2 || (argc >= 2 && strcmp(argv[1], "--help") ) == 0) {
+		exitWithHelp();
+	}
+
+	for ( int i = 1; i < argc; i++) {
+		if (argv[i][0] == '-') {
+			if (strcmp(argv[i], "-b") == 0 || strcmp(argv[i], "--number-nonblanck") == 0)
+					flags[0] = 1;
+			else if (strcmp(argv[i], "-e") == 0 || strcmp(argv[i], "-E") == 0)
+					flags[1] = 1;
+			else if (strcmp(argv[i], "-n") == 0 || strcmp(argv[i], "--number") == 0)
+					flags[2] = 1;
+			else if (strcmp(argv[i], "-s") == 0 || strcmp(argv[i], "--squeeze-blank") == 0)
+					flags[3] = 1;
+			else if (strcmp(argv[i], "-t") == 0 || strcmp(argv[i], "--T") == 0)
+					flags[4] = 1;
+			else 
+				exitWithHelp();
+		} else if (!f) {
+			f = readFile(argv[i]);
+		} else 
+			exitWithHelp();
+	}
+
 	if(f) {
 		returnContent(f, flags);
 	}
@@ -27,8 +55,13 @@ void returnContent(FILE* f, int flags[5]) {
 	int count_line = 1;
 	int count_nonline = 0;
 
-	char buff[256];
-	while ( fgets(buff, sizeof(buff), f) != NULL ) {
-		printf("%s", buff);
+	int ch;
+	while ( (ch = fgetc(f)) != EOF ) {
+		printf("%c", ch);
 	}
+}
+
+void exitWithHelp() {
+	printf("./cat [OPTIONS] [FILE]\n");
+	exit(1);
 }
