ca2205160fc6589c46fdbb82bd3a24af47285da5
kon-kyoto <kyotodanmaster@gmail.com>
2026-05-15 19:09:56 +0300

    feat: cat.c as main file for decomposition

---
+++ src/cat/cat.c
@@ -0,0 +1,14 @@
+#include "cat.h"
+
+int main(int argc, char* argv[]) {
+	char flags = 0;
+	FILE* f = NULL;
+
+	f = parse_flags(argc, argv, &flags, f);
+
+	if (f) {
+		print_content(f, &flags);
+	}
+
+	return 0;
+}
